f3ad9162fabcac9a444e9bdb46bcb4020e51eddc
Shradha Sharma <shradhasharma131@gmail.com>
2020-12-18 15:16:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'dill>=0.2.5',
         'docopt>=0.6.2',
-        'kafka-python==1.3.5'
+        'kafka-python==2.0.1'
     ],
     entry_points={
         'console_scripts': [
